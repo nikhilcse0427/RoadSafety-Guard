eacde5e594db2232d60f75ffc304238c5449fe93
nikhilcse0427 <nikhilcse0427@gmail.com>
2025-09-17 17:47:42 +0530

    deploy

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { body } = require('express-validator');
+const { body } = require('express-validator')
 const auth = require('../middleware/auth');
 const adminController = require('../controllers/adminController');
 
