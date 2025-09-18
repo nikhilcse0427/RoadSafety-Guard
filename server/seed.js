d6d21d00cb731559c7a3555fe21b585663752227
nikhilcse0427 <nikhilcse0427@gmail.com>
2025-09-18 14:59:54 +0530

    a

--- server/seed.js
+++ server/seed.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 import dotenv from 'dotenv';
 
 // Load environment variables
-dotenv.config();
+dotenv.config()
 
 // Import models
 const User = require('./models/User');
