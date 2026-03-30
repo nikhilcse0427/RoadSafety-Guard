276ec7864c44d54a08e50f6c334a8f4d595c4c52
Nikhil Verma <nikhilverma0427@gmail.com>
2026-03-30 21:07:59 +0530

    Final Vercel backend-frontend mapping

--- server/routes/accidents.js
+++ server/routes/accidents.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 const { body, validationResult } = require('express-validator');
 const Accident = require('../models/Accident');
 const auth = require('../middleware/auth');
